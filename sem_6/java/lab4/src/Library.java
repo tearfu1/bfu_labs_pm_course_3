fb6268c693effd8339da00200afc1c8aac1f63e1
Maxim Vayda <tearfu1@yandex.ru>
2025-05-09 11:45:45 +0200

    lab 4 done

--- sem_6/java/lab4/src/Library.java
+++ sem_6/java/lab4/src/Library.java
@@ -4,7 +4,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
+
 
 public class Library {
     private final List<Book> allBooks;
